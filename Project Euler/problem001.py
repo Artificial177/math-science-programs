a0933f0a672307861c99503d9e3015e57a210218
Amogh Joshi <67437306+amogh7joshi@users.noreply.github.com>
2020-11-14 11:58:11 -0500

    Update problem001.py

--- Project Euler/problem001.py
+++ Project Euler/problem001.py
@@ -1,3 +1,4 @@
+# My original solution.
 def sum_threefive(max_bound): # Problem #1
 
    result = 0
@@ -11,3 +12,6 @@ def sum_threefive(max_bound): # Problem #1
    return result
    
 print(sum_threefive(1000))
+
+# One Line Optimized Solution
+print(sum(i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0))
